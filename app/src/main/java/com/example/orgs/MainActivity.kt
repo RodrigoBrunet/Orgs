98bb9d254b0a5639c627347468fad57661c6c1f9
Rodrigo Brunet <rodrigosistemas2016@gmail.com>
2024-03-13 21:32:28 -0300

    implementando lista com recyclerview

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
-
+import android.widget.TextView
 
 
 class MainActivity : Activity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+       override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.produto_item)
+       val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de frutas"
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, manga, maçã"
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "19.99"
 
     }
 }
