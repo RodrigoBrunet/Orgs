26b1ac6009f272e6f1cd8f1c4e15a9b97a03ecd1
Rodrigo Brunet <rodrigosistemas2016@gmail.com>
2024-04-15 11:19:32 -0300

    finalizando adapter recyclerView

--- app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.R
+import com.example.orgs.model.Produto
 import com.example.orgs.ui.recyclerview.adapter.ListProdutosAdapter
+import java.math.BigDecimal
 
 
 class MainActivity : Activity() {
@@ -19,7 +22,22 @@ class MainActivity : Activity() {
         //val valor = findViewById<TextView>(R.id.valor)
         //valor.text = "19.99"
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-           recyclerView.adapter = ListProdutosAdapter()
+           recyclerView.adapter = ListProdutosAdapter(
+               context = this,
+               produtos = listOf(
+                   Produto(
+                       nome = "teste",
+                       descricao = "teste descr",
+                       valor = BigDecimal("19.99")
+                   ),
+                   Produto(
+                       nome = "teste2",
+                       descricao = "teste descr2",
+                       valor = BigDecimal("29.99")
+                   )
+               )
+           )
+
     }
 }
 
