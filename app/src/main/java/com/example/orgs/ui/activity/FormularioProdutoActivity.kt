bfd209437180f12e74d304c8d2ca72873a15b921
Rodrigo Brunet <rodrigosistemas2016@gmail.com>
2024-04-18 16:06:16 -0300

    refatoracao usando binding

--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,43 +2,52 @@ package com.example.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import com.example.orgs.R
 import com.example.orgs.dao.ProdutosDao
+import com.example.orgs.databinding.ActivityFormularioProdutoBinding
 import com.example.orgs.model.Produto
 import java.math.BigDecimal
 
-class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
-        botaoSalvar.setOnClickListener(View.OnClickListener {
-            val campoNome = findViewById<EditText>(R.id.nome)
-            val nome = campoNome.text.toString()
-            val campoDescricao = findViewById<EditText>(R.id.descricao)
-            val descricao = campoDescricao.text.toString()
-            val campoValor = findViewById<EditText>(R.id.valor)
-            val valorEmTexto = campoValor.text.toString()
-            val valor = if (valorEmTexto.isBlank()) {
-                BigDecimal.ZERO
-            } else {
-                BigDecimal(valorEmTexto)
-            }
-            val produtoNovo = Produto(
-                nome = nome,
-                descricao = descricao,
-                valor = valor
-            )
-            Log.i("FormularioProduto", "onCreate: $produtoNovo")
-            val dao = ProdutosDao()
-            dao.add(produtoNovo)
-            Log.i("FormularioProduto", "onCreate: ${dao.listarTodos()}")
-            finish()
-        })
+class FormularioProdutoActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        ActivityFormularioProdutoBinding.inflate(layoutInflater)
+    }
+       override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        configuraBotaoSalvar()
+
+    }
+
+    private fun configuraBotaoSalvar() {
+        val botaoSalvar = binding.activityFormularioProdutoBotaoSalvar
+        botaoSalvar.setOnClickListener(View.OnClickListener {
+           val produtoNovo = criaProduto()
+           val dao = ProdutosDao()
+           dao.add(produtoNovo)
+           finish()
+        })
+    }
+
+    private fun criaProduto(): Produto {
+        val campoNome = binding.activityFormularioProdutoNome
+        val nome = campoNome.text.toString()
+        val campoDescricao = binding.activityFormularioProdutoDescricao
+        val descricao = campoDescricao.text.toString()
+        val campoValor = binding.activityFormularioProdutoValor
+        val valorEmTexto = campoValor.text.toString()
+        val valor = if (valorEmTexto.isBlank()) {
+            BigDecimal.ZERO
+        } else {
+            BigDecimal(valorEmTexto)
+        }
+        return Produto(
+            nome = nome,
+            descricao = descricao,
+            valor = valor
+        )
     }
 }
---
+++ app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
@@ -0,0 +1,49 @@
+package com.example.orgs.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.orgs.R
+import com.example.orgs.dao.ProdutosDao
+import com.example.orgs.databinding.ActivityListaProdutosBinding
+import com.example.orgs.ui.recyclerview.adapter.ListProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
+class ListaProdutosActivity : AppCompatActivity() {
+
+    private val dao = ProdutosDao()
+    private val adapter = ListProdutosAdapter(
+        context = this,
+        produtos = dao.listarTodos()
+    )
+    private val binding by lazy {
+        ActivityListaProdutosBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        configuraRecyclerView()
+        configuraFab()
+    }
+    override fun onResume() {
+        super.onResume()
+        adapter.atualiza(dao.listarTodos())
+
+    }
+
+    private fun configuraFab() {
+        val fab = binding.activityListaProdutosFab
+        fab.setOnClickListener {
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun configuraRecyclerView() {
+        val recyclerView = binding.activityListaProdutosRecyclerView
+        recyclerView.adapter = adapter
+    }
+}
+
